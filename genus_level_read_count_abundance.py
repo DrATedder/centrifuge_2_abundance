64422098b41b44553e348032cb07c8dd4fdeb489
Andrew Tedder <54320061+DrATedder@users.noreply.github.com>
2023-10-20 14:51:44 +0100

    Update genus_level_read_count_abundance.py
    User select 'genus' or 'species' abundance output

--- genus_level_read_count_abundance.py
+++ genus_level_read_count_abundance.py
@@ -2,48 +2,58 @@ import sys
 import os
 import glob
 
-def cumulative_reads(cent_out):
-    cumulative_genus = {}
+def cumulative_taxa(cent_out, level):
+    cumulative_reads = {}
+    taxon_keyword = "species" if level == "species" else "genus"
+    
     for line in cent_out:
-        if not line.startswith("name"):
-            if "species" in line or "genus" in line or "leaf" in line:
-                parts = line.split("\t")
-                genus, uniqR = parts[0].split()[0], parts[4]
-                cumulative_genus.setdefault(genus, []).append(int(uniqR))
-    return cumulative_genus
+        if not line.startswith("name") and taxon_keyword in line:
+            parts = line.split("\t")
+            if len(parts[0].split()) > 1:
+                taxon_name = parts[0].split()[0] + " " + parts[0].split()[1] if level == "species" else parts[0].split()[0]
+                uniq_reads = int(parts[4])
+                cumulative_reads.setdefault(taxon_name, []).append(uniq_reads)
+            else:
+                taxon_name = parts[0].split()[0]
+                uniq_reads = int(parts[4])
+                cumulative_reads.setdefault(taxon_name, []).append(uniq_reads)
+    
+    return cumulative_reads
 
-def process_file(input_file):
+def process_file(input_file, abundance_threshold, level):
     with open(input_file, "r") as f_in:
-        cumulative_genus = cumulative_reads(f_in)
+        cumulative_reads = cumulative_taxa(f_in, level)
 
-    total = sum(sum(values) for values in cumulative_genus.values())
+    total = sum(sum(values) for values in cumulative_reads.values())
 
-    # Produce a list with each genus, total unique read count, and abundance.
     abundance_list = []
-    for genus, values in cumulative_genus.items():
+    for taxon, values in cumulative_reads.items():
         abundance = (sum(values) / total) * 100
         if abundance >= float(abundance_threshold):
-            abundance_list.append("{0},{1},{2:.2f}".format(genus, sum(values), abundance))
+            abundance_list.append(f"{taxon},{sum(values)},{abundance:.2f}")
 
     if abundance_list:
-        output_file = os.path.splitext(input_file)[0] + "_abundance.txt"
+        output_file = os.path.splitext(input_file)[0] + f"_{level}_abundance.txt"
         with open(output_file, "w") as f_out:
             for result in abundance_list:
                 f_out.write(result + '\n')
 
-        print("Results written to", output_file)
+        print(f"Results written to {output_file}")
 
-def main(input_dir, abundance_threshold):
+def main(input_dir, abundance_threshold, level):
     for input_file in glob.glob(os.path.join(input_dir, '*centrifugeReport.txt')):
-        process_file(input_file)
+        process_file(input_file, abundance_threshold, level)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python genus_level_read_count_abundance.py input_directory abundance_threshold")
+    if len(sys.argv) != 4:
+        print("Usage: python species_level_read_count_abundance.py input_directory abundance_threshold level[species or genus]")
         sys.exit(1)
+    
     input_dir = sys.argv[1]
     abundance_threshold = sys.argv[2]
-    main(input_dir, abundance_threshold)
+    level = sys.argv[3].lower()
+    main(input_dir, abundance_threshold, level)
+
 
 
 
